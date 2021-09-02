7925c06c4c24d6ee91978cd3da8ffe5f3d426308
Will Ferrell <waf0006@mix.wvu.edu>
2021-09-02 09:24:35 -0400

    Handler!

--- mpx_core_2020/mpx_core/modules/CommandHandler.c
+++ mpx_core_2020/mpx_core/modules/CommandHandler.c
@@ -1,1 +1,23 @@
 // Command Handler Implementation
+
+#include <system.h>
+#include <kernel/core/serial.c>
+#include "CommandHandler.h"
+#include <string.h>
+
+intcomhand(){
+  charcmdBuffer[100];
+  int bufferSize;
+  int quit=0;
+
+  while(!quit) {
+  // get acommand
+  memset(buffer,‘\0’,   100);
+  bufferSize = 99;
+  // reset size before each call to read
+  sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize);
+
+  // process the command
+  // see if quit was entered
+}
+}
