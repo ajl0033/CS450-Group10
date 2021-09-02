18fefd0db02639851f9a738c8ff96cb3aae34b87
Will Ferrell <waf0006@mix.wvu.edu>
2021-09-02 09:06:02 -0400

    Handler Header

--- mpx_core_2020/mpx_core/modules/CommandHandler.h
+++ mpx_core_2020/mpx_core/modules/CommandHandler.h
@@ -1,1 +1,41 @@
 // Header for Command Handler
+
+#include <system.h>
+
+/*
+  Prints the current version of the MPX and the completion date
+*/
+void version() {
+
+}
+
+/*
+  Provides usage instructions for each command
+*/
+void help() {
+
+}
+
+/*
+  Exits the command handler loop
+  Execution will return to kmain() and the system will halt
+*/
+void shutdown() {
+
+}
+
+void getdate() {
+
+}
+
+void setdate() {
+
+}
+
+void gettime() {
+
+}
+
+void settime() {
+
+}
