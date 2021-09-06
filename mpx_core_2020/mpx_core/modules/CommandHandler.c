c31366d04544f80f8e71fb60814bf9374ff437ef
willf2000 <waf0006@mix.wvu.edu>
2021-09-06 19:46:14 -0400

    Handler Implementation

--- mpx_core_2020/mpx_core/modules/CommandHandler.c
+++ mpx_core_2020/mpx_core/modules/CommandHandler.c
@@ -1,30 +1,86 @@
 // Command Handler Implementation
-
 #include <system.h>
-#include <core/serial.h>
+#include <kernel/core/serial.h>
+#include <kernel/core/serial.c>
+#include <mpx_supt.c>
 #include "CommandHandler.h"
 #include <string.h>
-#include "mpx_supt.h"
-#include "print.h"
-#include <core/io.h>
-#include "time.h"
 
-//jaretts push
-
-int comhand(){
-  char cmdBuffer[100];
+intcomhand(){
+  charcmdBuffer[100];
   int bufferSize;
   int quit=0;
+  char welcome[] = "Welcome to the MPX! What would you like to do?\n1: Help\n2: Version\n3: Get Date\n4: Set Date\n5: Get Time\n6: Set Time\n7: Shutdown";
 
   while(!quit) {
-  // get acommand
-  memset(cmdBuffer,'\0',   100);
+    char * strcpy(char* charcmdBuffer, const char* welcome, size_t welcome);
+    sys_req(WRITE, DEFAULT_DEVICE, charcmdBuffer, &bufferSize);
+  // get a command
+  memset(buffer,‘\0’,   100);
   bufferSize = 99;
   // reset size before each call to read
   sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize);
-
+  if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 1) {
+    help();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 2) {
+    version();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 3) {
+    getdate();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 4) {
+    setdate();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 5) {
+    gettime();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 6) {
+    settime();
+  }
+  else if (sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize) == 7) {
+    shutdown();
+  }
   // process the command
   // see if quit was entered
+}
+void version() {
+  char version[] = "R1, 9/5/2021";
+  char * strcpy(char* charcmdBuffer, const char* version, size_t version);
+  sys_req(WRITE,DEFAULT_DEVICE,charcmdBuffer,&bufferSize);
+}
+
+void help() {
+
+}
+
+int shutdown() {
+  char shutdown[] = "Are you sure you want to quit and shutdown? Y: 1, N: 2";
+  char * strcpy(char* charcmdBuffer, const char* shutdown, size_t shutdown);
+  sys_req(WRITE,DEFAULT_DEVICE,charcmdBuffer,&bufferSize);
+  int quit;
+  scanf("%d", &quit);
+  if (quit == 1) {
+    return quit;
   }
-  return 0;
+  else if (quit == 2) {
+    return quit;
+  }
+}
+
+void getdate() {
+
+}
+
+void setdate() {
+
+}
+
+void gettime() {
+
+}
+
+void settime() {
+
+}
 }
