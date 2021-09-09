815da23adb3a90e5a87d539bcb4e1e1363342b27
jarett <jh0124@mix.wvu.edu>
2021-09-09 06:33:57 -0400

    mm/dd/yyyy

--- mpx_core_2020/mpx_core/modules/date.c
+++ mpx_core_2020/mpx_core/modules/date.c
@@ -20,13 +20,13 @@ int getdate(){
 char date[11] = "00/00/0000";
 
 
-//dayOfMonth
-outb(0x70, 0x07);
- BCDtoStr(inb(0x71), &date[0]);
-
 //Month
 outb(0x70, 0x08);
-BCDtoStr(inb(0x71), &date[3]);
+BCDtoStr(inb(0x71), &date[0]);
+
+//dayOfMonth
+outb(0x70, 0x07);
+ BCDtoStr(inb(0x71), &date[3]);
 
 //century
 outb(0x70, 0x32);
@@ -40,7 +40,7 @@ BCDtoStr(inb(0x71), &date[8]);
 // outb(0x70, 0x06);
 // BCDtoStr(inb(0x71), &date[10]);
 
-print("\n\nCurrent Date formatted dayOfMonth/Month/year is: ");
+print("\n\nCurrent Date formatted Month/dayOfMonth/year is: ");
 println(date);
 return 0;
 
@@ -53,8 +53,8 @@ return 0;
    int bufferSize;
    memset(cmdBuffer, '\0', 100);
    bufferSize = 99;
-   print("\n\nEnter a date in the form dayOfMonth/Month/year\n");
-   print("Example date could be \"04/02/1999\" - or February 4th / 1999 \n\n");
+   print("\n\nEnter a date in the form Month/dayOfMonth/year\n");
+   print("Example date could be \"02/04/1999\" - or February 4th / 1999 \n\n");
   sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize);
   char date[11];
 
@@ -63,20 +63,21 @@ return 0;
     date[count] = cmdBuffer[count];
     count++;
   }
-//0402/2002
-//0123/5678
+// 04/02/2002
+// 01/34/6789
 //need to add more error checking
 if(checkDate(date)){
   return 1;
 }else{
   cli();
 
-//dayOfMonth
-outb(0x70, 0x07);
-outb(0x71, StrtoBCD(&date[0]));
 
 //month
 outb(0x70, 0x08);
+outb(0x71, StrtoBCD(&date[0]));
+
+//dayOfMonth
+outb(0x70, 0x07);
 outb(0x71, StrtoBCD(&date[3]));
 
 //century
@@ -91,7 +92,7 @@ outb(0x71, StrtoBCD(&date[8]));
 // outb(0x70, 0x06);
 // outb(0x71, StrtoBCD(&date[10]));
 
-print("\n\nDate has been set in the form dayOfMonth/Month/year to: ");
+print("\n\nDate has been set in the form Month/dayOfMonth/year to: ");
 print(date);
 println("");
 sti();
@@ -101,13 +102,13 @@ return 0;
 }
 // 04/02/1999
 // 01/34/6789
-//dayOfMonth/Month/year/dayOfWeek
+// Month/dayOfMonth/year
 int checkDate(char* date){
   if(
      date[2] != '/' ||
-     date[5] != '/' || date[0] > '3'
+     date[5] != '/' || date[0] > '1'
   || date[0] < '0'  || date[1] < '0'
-  || date[1] > '9'  || date[3] > '1'
+  || date[1] > '9'  || date[3] > '3'
 )  {
 
   println("\n\nDate is invalid format...");
