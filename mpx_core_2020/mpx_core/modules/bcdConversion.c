ffdff32bdbda4ae34c75dc2990b5b2640793f985
jarett <jh0124@mix.wvu.edu>
2021-09-10 00:11:21 -0400

    bcdConv documen

--- mpx_core_2020/mpx_core/modules/bcdConversion.c
+++ mpx_core_2020/mpx_core/modules/bcdConversion.c
@@ -3,26 +3,41 @@
 #include "print.h"
 #include <core/io.h>
 
+/*
+intToBCD:
+take in a int value as an arguement
+returns a BCD given the int value
+*/
 int intToBCD(int val){
  return ((val/10)<<4 ) | (val % 10);
 }
 
+/*
+BCDtoInt:
+takes in a int BCD value as an arguement
+returns an int given the BCD value
+*/
 int BCDtoInt(int val){
  return ((val & 0xF0)>> 4)*10 + (val & 0x0F);
 }
+
+/*
+BCDtoStr:
+takes in a BCD and a char array pointer as an arguement
+returns 0 but puts the BCD into the char array
+*/
 int BCDtoStr(int val,char* str){
 //need to add another 2 chars somehow to get year to work otherwise everyhting works
   str[0] = (val >> 4) + '0';
   str[1] = (val & 0x0F) + '0';
-  // if(str[2] >= 48 && str[2]<=57){
-  //   str[2] = (val >> 4) + '0';
-  // }
-  // if(str[3] >= 48 && str[2]<=57){
-  //   str[3] = (val & 0x0F) + '0';
-  // }
   return 0;
 }
 
+/*
+StrtoBCD:
+takes in a char array pointer as an arguement
+returns the BCD of the number that was in the char array
+*/
 int StrtoBCD(char* str){
   int bcd;
   bcd = ((str[0] -'0') << 4) + (str[1]- '0');
