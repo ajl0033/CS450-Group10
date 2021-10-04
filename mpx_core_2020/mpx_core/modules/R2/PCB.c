9e7d92f03b961bfa8e02b2da8f482352d4d369f7
willf2000 <waf0006@mix.wvu.edu>
2021-10-04 19:32:17 -0400

    Some stuff

--- mpx_core_2020/mpx_core/modules/R2/PCB.c
+++ mpx_core_2020/mpx_core/modules/R2/PCB.c
@@ -3,7 +3,16 @@
 #include <system.h>
 #include <core/serial.h>
 #include "PCB.h"
+#include <Queue.c>
 
+Struct ready = CreateQueue();  // Is this how you do it??? Hahaha
+int readySize = 0;
+Struct blocked = CreateQueue();
+int blockedSize = 0;
+Struct SuspendedReady = CreateQueue();
+int SuspendedReadySize = 0;
+Struct SuspendedBlocked = CreatQueue();
+int SuspendedBlockedSize;
 
 PCB* AllocatePCB()
 {
@@ -30,6 +39,10 @@ PCB* FindPCB(char* processName)
   // Search each queue for process. Null if not found, else return PCB.
   // How do you access the queues, and how are the queues initialized.
   // Will will figure this out at some point! Studying right now for 450 and 320!
+  for (int i = 0; i < readySize; i++)
+  {
+    if (ready)
+  }
   return null;
 }
 
