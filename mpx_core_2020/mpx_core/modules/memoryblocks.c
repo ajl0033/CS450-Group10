9dee4f9b967b478f5dd19e9a5221dd19ef52d4d1
Emily Waechter <ecw0006@mix.wvu.edu>
2021-10-22 18:43:29 -0400

    creating control blocks and new c file module 5

---
+++ mpx_core_2020/mpx_core/modules/memoryblocks.c
@@ -0,0 +1,6 @@
+#include <string.h>
+#include <system.h>
+#include "mpx_supt.h"
+#include <core/serial.h>
+#include "memoryblocks.h"
+#include "print.h"
---
+++ mpx_core_2020/mpx_core/modules/memoryblocks.h
@@ -0,0 +1,25 @@
+#ifndef _MEMORYBLOCKS_H
+#define _MEMORYBLOCKS_H
+#include <system.h>
+
+typedef struct CMCB{
+  int type; //free is 0 and allocated is 1
+  int beginningAddress;
+  int size;
+  char name[20];
+  struct PCB *nextCMCB;
+  struct PCB *previousCMCB;
+} CMCB;
+
+typedef struct LMCB{
+  int type; //free is 0 and allocated is 1
+  int size;
+} LMCB;
+
+void initialize_heap(int size);
+void allocate_memory(int bytes);
+void free_memory();
+
+
+
+#endif
