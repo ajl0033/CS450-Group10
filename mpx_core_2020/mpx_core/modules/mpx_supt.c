7684387c2b96574165913442fed930cbbc9d0728
Emily Waechter <ecw0006@mix.wvu.edu>
2021-10-21 22:30:54 -0400

    fun

--- mpx_core_2020/mpx_core/modules/mpx_supt.c
+++ mpx_core_2020/mpx_core/modules/mpx_supt.c
@@ -220,15 +220,15 @@ void yield(){
 	asm volatile("int $60");
 }
 void loadr3(){
-PCB* new_pcb = create_pcb (name , 1 , 1 , 1 , stack_size );
-context* cp = (context *)(new_pcb->stack_top );
-memset (cp , 0, sizeof (context ));
+PCB* new_pcb = CreatePCB(name, 1 , 1);
+context* cp = (context *)(new_pcb->stackTop);
+memset (cp , 0, sizeof (context *));
 cp->fs = 0x10;
 cp->gs = 0x10;
 cp->ds = 0x10;
 cp->es = 0x10;
 cp->cs = 0x8;
-cp->ebp = ( u32int )( new_pcb->stack );
-cp->esp = ( u32int )( new_pcb->stack_top );
+cp->ebp = (u32int*)( new_pcb->stack );
+cp->esp = (u32int)( new_pcb->stackTop );
 cp->eip = ( u32int )func;// The function correlating to the process , ie. Proc1cp - > eflags = 0 x202 ;return new_pcb ;
 }
