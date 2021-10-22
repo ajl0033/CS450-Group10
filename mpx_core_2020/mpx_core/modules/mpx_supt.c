018b348c7c6bbd4331eba92ae2bd4ba352d357c5
jarett <jh0124@mix.wvu.edu>
2021-10-21 22:12:17 -0400

    loadr3 and remove vimp from tables

--- mpx_core_2020/mpx_core/modules/mpx_supt.c
+++ mpx_core_2020/mpx_core/modules/mpx_supt.c
@@ -219,3 +219,16 @@ else{
 void yeild(){
 	asm volatile("int $60");
 }
+void loadr3(){
+pcb * new_pcb = create_pcb ( name , 1 , 1 , 1 , stack_size );
+context * cp = ( context *)( new_pcb -> stack_top );
+memset ( cp , 0, sizeof ( context ));
+cp - > fs = 0 x10;
+cp - > gs = 0 x10;
+cp - > ds = 0 x10;
+cp - > es = 0 x10;
+cp - > cs = 0 x8;
+cp - > ebp = ( u32int )( new_pcb -> stack );
+cp - > esp = ( u32int )( new_pcb -> stack_top );
+cp - > eip = ( u32int ) func ;// The function correlating to the process , ie. Proc1cp - > eflags = 0 x202 ;return new_pcb ;
+}
