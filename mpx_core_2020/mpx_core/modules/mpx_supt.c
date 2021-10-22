bcdf42e6de947288df0859e633d405f823743ac1
jarett <jh0124@mix.wvu.edu>
2021-10-21 22:34:03 -0400

    ojhhh2

--- mpx_core_2020/mpx_core/modules/mpx_supt.c
+++ mpx_core_2020/mpx_core/modules/mpx_supt.c
@@ -231,6 +231,6 @@ cp->cs = 0x8;
 cp->ebp = (u32int)( new_pcb->stack );
 cp->esp = (u32int)( new_pcb->stackTop );
 cp->eip = ( u32int )func;// The function correlating to the process , ie. Proc1
-cp - > eflags = 0 x202 ;
+cp->eflags = 0 x202 ;
 return new_pcb ;
 }
