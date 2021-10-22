133ba224dc37189f0f4948016a4d0674b9397c0c
willf2000 <waf0006@mix.wvu.edu>
2021-10-21 23:16:37 -0400

    header alarm

--- mpx_core_2020/mpx_core/modules/Alarm.h
+++ mpx_core_2020/mpx_core/modules/Alarm.h
@@ -3,9 +3,6 @@
 #include <system.h>
 #include <core/serial.h>
 
-void createAlarmProcess() // Create Process
-{
+void createAlarmProcess(); // Create Process
 
-void addAlarm(char* message, int alarmTime) // Command for terminal
-{
-}
+void addAlarm(char* message, int alarmTime); // Command for terminal
