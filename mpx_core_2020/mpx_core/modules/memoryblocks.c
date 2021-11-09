df5bb63c4da21163ad42b0bc7f697d3edab08780
willf2000 <waf0006@mix.wvu.edu>
2021-11-09 15:34:25 -0500

    free memory start

--- mpx_core_2020/mpx_core/modules/memoryblocks.c
+++ mpx_core_2020/mpx_core/modules/memoryblocks.c
@@ -18,7 +18,39 @@ void allocate_memory(int bytes)
 
 }
 
-void free_memory(int beginningAddress) // Will
+void free_memory(int address) // Will, trying to wrap head around it, decent start, need initialize function to visualize
 {
-  while ()
+  CMCB* tempAllocated = allocated.head;
+  while (tempAllocated != NULL)
+  {
+    if (tempAllocated->beginningAddress == address)
+    {
+      // Allocate to free list
+      allocate_memory(tempAllocated->size);
+      // Remove from allocated list
+      if(tempAllocated == allocated->head){
+        allocated->head = tempAllocated->nextCMCB;
+      }
+      //else if it is at the tail,
+      else if(tempAllocated == allocated->tail){
+        allocated->tail = tempAllocated->previousCMCB;
+        allocated->tail->nextCMCB = NULL;
+      }
+      else{
+        tempAllocated->previousCMCB->nextCMCB = tempAllocated->nextCMCB;
+        tempAllocated->nextCMCB->previousCMCB = tempAllocated->previousCMCB;
+      }
+      tempAllocated->nextCMCB = NULL;
+      tempAllocated->previousCMCB = NULL;
+      allocated->count--;
+
+      // Check if blocks of free memory need merged
+      // if address adjacent in heap contains CMCB of same type (free==0, allocated==1), merge blocks
+      // nextCMCB = CMCB, remove a CMCB and LCMB
+      // size: sizeof(both blocks of memory) + 2*sizeof(CMCB) + 2*sizeof(LCMB) ---->>>> sizeof(both blocks of memory) + sizeof(CMCB) + sizeof(LCMB)
+
+      break;
+    }
+    tempAllocated = tempAllocated->nextCMCB;
+  }
 }
