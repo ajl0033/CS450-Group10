6d329913180bef2003db5d4bd9c379f75872f301
willf2000 <waf0006@mix.wvu.edu>
2021-12-07 11:02:27 -0500

    Added Errors given

--- mpx_core_2020/mpx_core/modules/serial.h
+++ mpx_core_2020/mpx_core/modules/serial.h
@@ -3,6 +3,21 @@
 
 #include "dcb.h"
 
+#define OPEN_INVALID_EVENT_FLAG -101
+#define OPEN_INVALID_BAUD_RATE -102
+#define OPEN_PORT_IS_OPEN -103
+#define CLOSE_PORT_NOT_OPEN -201
+#define READ_PORT_NOT_OPEN -301
+#define READ_INVALID_BUFFER_ADDR -302
+#define READ_INVALID_COUNT -303
+#define READ_DEVICE_BUSY -304
+#define WRITE_PORT_NOT_OPEN -401
+#define WRITE_INVALID_BUFFER_ADDR -402
+#define WRITE_INVALID_COUNT -403
+#define WRITE_DEVICE_BUSY -404
+
+// Returns the device control block associated with the serial port
+// struct dcb* com_get_dcb();
 
 void set_int(int bit, int on);
 
